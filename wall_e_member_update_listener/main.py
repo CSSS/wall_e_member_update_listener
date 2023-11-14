c8e33dc4f2bda6953d1afebb0a07fc452ab66401
Jace Manshadi <jaymanshad@proton.me>
2023-11-13 20:43:44 -0800

    small fixes

--- wall_e_member_update_listener/main.py
+++ wall_e_member_update_listener/main.py
@@ -1,10 +1,15 @@
+import logging
 import os
 
 import django
 from django.core.wsgi import get_wsgi_application
 
-from discordpy_stream_handler import DiscordPyDebugStreamHandler
-from loggers import member_update_listener_log
+
+from setup_logger import Loggers
+
+log_info = Loggers.get_logger(logger_name="member_update_listener")
+member_update_listener_log = log_info[0]
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_settings")
 django.setup()
@@ -19,6 +24,17 @@ from wall_e_models.models import UpdatedUser
 
 
 bot = commands.Bot(command_prefix='.', intents=Intents.all())
+
+
+@bot.listen(name="on_ready")
+async def ready():
+    member_update_listener_log.info("member update listener is now up")
+
+
+@bot.listen(name="on_command_error")
+async def func(ctx, error):
+    if type(error) != commands.CommandNotFound:
+        member_update_listener_log.error(error)
 
 
 @bot.listen(name="on_member_update")
@@ -39,8 +55,25 @@ async def new_member(member: discord.Member):
 async def mark_user_as_updated(member: discord.Member):
     user_to_update = await UpdatedUser.user_update_is_unique(member)
     if user_to_update is not None:
-        member_update_listener_log.info(f"marking user {member} as needed an update")
+        member_update_listener_log.info(f"marking user {member} as needing an update")
         await UpdatedUser(user_point=user_to_update).async_save()
 
+
+discordpy_logger_name = "discord.py"
+
+
+class DiscordPyDebugStreamHandler(logging.StreamHandler):
+    def __init__(self):
+        super(DiscordPyDebugStreamHandler, self).__init__()
+
+    def emit(self, record):
+        if record.name != discordpy_logger_name:
+            for handler in discordpy_logger.handlers:
+                if record.levelno >= handler.level:
+                    handler.emit(record)
+
+
+discordpy_log_info = Loggers.get_logger(logger_name=discordpy_logger_name)
+discordpy_logger = discordpy_log_info[0]
 
 bot.run(token=os.environ["basic_config__TOKEN"], log_handler=DiscordPyDebugStreamHandler())
--- wall_e_member_update_listener/setup_logger.py
+++ wall_e_member_update_listener/setup_logger.py
@@ -5,7 +5,6 @@ import sys
 
 import pytz
 
-SYS_LOG_HANDLER_NAME = "sys"
 date_timezone = pytz.timezone('US/Pacific')
 date_formatting_in_filename = "%Y_%m_%d_%H_%M_%S"
 error_logging_level = logging.ERROR
@@ -63,77 +62,7 @@ class Loggers:
         :param logger_name: the name to assign to the returned logic
         :return:the logger
         """
-        if logger_name == SYS_LOG_HANDLER_NAME:
-            return cls._setup_sys_logger()
-        else:
-            return cls._setup_logger(logger_name)
-
-    @classmethod
-    def _setup_sys_logger(cls):
-        """
-        Creates a sys logger that directs anything going to sys.stdout/err to a log file
-        and the stream as well
-        :return: the sys logger
-        """
-        date = datetime.datetime.now(date_timezone).strftime(date_formatting_in_filename)
-        if not os.path.exists(f"logs/{SYS_LOG_HANDLER_NAME}"):
-            os.makedirs(f"logs/{SYS_LOG_HANDLER_NAME}")
-        if not os.path.exists(f"logs/{SYS_LOG_HANDLER_NAME}"):
-            os.makedirs(f"logs/{SYS_LOG_HANDLER_NAME}")
-
-        sys_logger = logging.getLogger(SYS_LOG_HANDLER_NAME)
-        sys_logger.setLevel(logging.DEBUG)
-
-        debug_log_file_absolute_path = (
-            f"logs/{SYS_LOG_HANDLER_NAME}/{date}_debug.log"
-        )
-        sys_stream_warn_log_file_absolute_path = (
-            f"logs/{SYS_LOG_HANDLER_NAME}/{date}_warn.log"
-        )
-        sys_stream_error_log_file_absolute_path = (
-            f"logs/{SYS_LOG_HANDLER_NAME}/{date}_error.log"
-        )
-
-        # ensures that anything printed to this logger at level DEBUG or above goes to the specified file
-        debug_filehandler = logging.FileHandler(debug_log_file_absolute_path)
-        debug_filehandler.setLevel(logging.DEBUG)
-        sys_logger.addHandler(debug_filehandler)
-
-        # ensures that anything printed to this logger at level WARN or above goes to the specified file
-        warn_filehandler = logging.FileHandler(sys_stream_warn_log_file_absolute_path)
-        warn_filehandler.setLevel(warn_logging_level)
-        sys_logger.addHandler(warn_filehandler)
-
-        # ensures that anything printed to this logger at level ERROR or above goes to the specified file
-        error_filehandler = logging.FileHandler(sys_stream_error_log_file_absolute_path)
-        error_filehandler.setLevel(error_logging_level)
-        sys_logger.addHandler(error_filehandler)
-
-        # ensures that anything from the log goes to the stdout
-        if REDIRECT_STD_STREAMS:
-            sys.stdout = sys.__stdout__
-        sys_stdout_stream_handler = WalleDebugStreamHandler(sys.stdout)
-        sys_stdout_stream_handler.setLevel(logging.DEBUG)
-        sys_logger.addHandler(sys_stdout_stream_handler)
-        if REDIRECT_STD_STREAMS:
-            sys.stdout = LoggerWriter(sys_logger.info)
-
-        sys_std_warn_stream_handler = WalleWarnStreamHandler(sys.stdout)
-        sys_std_warn_stream_handler.setLevel(warn_logging_level)
-        sys_logger.addHandler(sys_std_warn_stream_handler)
-
-        if REDIRECT_STD_STREAMS:
-            sys.stderr = sys.__stderr__
-        sys_stderr_stream_handler = logging.StreamHandler(sys.stderr)
-        sys_stderr_stream_handler.setLevel(error_logging_level)
-        sys_logger.addHandler(sys_stderr_stream_handler)
-        if REDIRECT_STD_STREAMS:
-            sys.stderr = LoggerWriter(sys_logger.error)
-
-        return (
-            sys_logger, debug_log_file_absolute_path, sys_stream_warn_log_file_absolute_path,
-            sys_stream_error_log_file_absolute_path
-        )
+        return cls._setup_logger(logger_name)
 
     @classmethod
     def _setup_logger(cls, service_name):
